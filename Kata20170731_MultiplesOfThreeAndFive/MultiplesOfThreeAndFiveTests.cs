975272a0459b33995f238000425deebe969ebf28
cash <cash@cashwu.com>
2017-07-31 21:25:37 +0800

    input 4 test case

--- Kata20170731_MultiplesOfThreeAndFive/MultiplesOfThreeAndFiveTests.cs
+++ Kata20170731_MultiplesOfThreeAndFive/MultiplesOfThreeAndFiveTests.cs
@@ -7,8 +7,32 @@ namespace Kata20170731_MultiplesOfThreeAndFive
     public class MultiplesOfThreeAndFiveTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_4_should_return_3()
         {
+            AssertMultiple3And5ShouldBe();
+        }
+
+        private static void AssertMultiple3And5ShouldBe()
+        {
+            var kata = new Kata();
+            var actual = kata.Solution(4);
+            Assert.AreEqual(3, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int Solution(int value)
+        {
+            for (int i = 1; i < 4; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    return i;
+                }
+            }
+
+            return 0;
         }
     }
 }
