56c97efc356521e44edab08af121aa22ce7add72
cash <cash@cashwu.com>
2017-07-31 21:28:16 +0800

    input 6 test case

--- Kata20170731_MultiplesOfThreeAndFive/MultiplesOfThreeAndFiveTests.cs
+++ Kata20170731_MultiplesOfThreeAndFive/MultiplesOfThreeAndFiveTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20170731_MultiplesOfThreeAndFive
@@ -10,6 +12,12 @@ namespace Kata20170731_MultiplesOfThreeAndFive
         public void input_4_should_return_3()
         {
             AssertMultiple3And5ShouldBe(4, 3);
+        }
+
+        [TestMethod]
+        public void input_6_should_return_8()
+        {
+            AssertMultiple3And5ShouldBe(6, 8);
         }
 
         private static void AssertMultiple3And5ShouldBe(int value, int expected)
@@ -24,15 +32,16 @@ namespace Kata20170731_MultiplesOfThreeAndFive
     {
         public int Solution(int value)
         {
-            for (int i = 1; i < 4; i++)
+            var result = new List<int>();
+            for (int i = 1; i < value; i++)
             {
-                if (i % 3 == 0)
+                if (i % 3 == 0 || i % 5 == 0)
                 {
-                    return i;
+                    result.Add(i);
                 }
             }
 
-            return 0;
+            return result.Sum();
         }
     }
 }
